bc23f314bcb24fa6b2a2b4287b5efd43fa1a572d
root <root@ooper.space>
2017-11-25 15:24:15 -0500

    Mild Fix

--- demo/server.php
+++ demo/server.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'vendor/autoload.php';
-$r = json_decode(file_get_contents("https://newsapi.org/v2/everything?q=" . $_GET['q'] . "&apiKey=297a2382d51e4302a38b6a38fa167b7a"));
+$r = json_decode(file_get_contents("https://newsapi.org/v2/everything?q=" . urlencode($_GET['q']) . "&apiKey=297a2382d51e4302a38b6a38fa167b7a"));
 $s = "";
 foreach ($r->articles as $i) {
 	$s .= $i->description . " ";
